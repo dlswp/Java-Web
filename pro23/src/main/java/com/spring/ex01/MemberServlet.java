32b6b44b0f86b7fa442b8d1837665a206012f15b
Park InJe <akil120@naver.com>
2023-05-21 15:49:51 +0900

    0521 commit

--- pro23/src/main/java/com/spring/ex01/MemberServlet.java
+++ pro23/src/main/java/com/spring/ex01/MemberServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MemberServlet
  */
-@WebServlet("/mem.do")
+//@WebServlet("/mem.do")
 public class MemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
--- pro23/src/main/java/com/spring/ex02/MemberServlet.java
+++ pro23/src/main/java/com/spring/ex02/MemberServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MemberServlet
  */
-@WebServlet("/mem2.do")
+//@WebServlet("/mem2.do")
 public class MemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
